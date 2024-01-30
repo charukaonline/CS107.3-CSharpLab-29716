943244a9b1f87611e35f5ad810e401bbb7965a8f
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-01-30 15:11:52 +0530

    lab sheet 1 - q4

---
+++ tutorial-1/tutorial1_q4/tutorial1_q4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the value of N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
