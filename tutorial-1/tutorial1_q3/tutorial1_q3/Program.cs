cae0b5a63d8e53d22cf53688a29b268af9cdedfb
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-01-30 15:08:53 +0530

    lab sheet 1 - q1, q2, q3

---
+++ tutorial-1/tutorial1_q3/tutorial1_q3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tutorial1_q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive integer:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("ERROR");
+            }
+            else
+            {
+                int sum = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine("Sum of numbers from 1 to {0} is {1}", number, sum);
+            }
+        }
+    }
+}
