abb9c812c09425d13e5427346648caac41e80c6a
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-01-30 15:15:33 +0530

    lab sheet 1 - q5

---
+++ tutorial-1/tutorial1_q5/tutorial1_q5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Multiplication Table of {0}:", number);
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine("{0} x {1} = {2}", number, i, result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
