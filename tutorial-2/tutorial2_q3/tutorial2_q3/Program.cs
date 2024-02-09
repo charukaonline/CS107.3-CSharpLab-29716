e6665e943dbcd6d073860cfcaab6ea0e58b51cf6
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-02-09 11:01:28 +0530

    tutorial2_q3

---
+++ tutorial-2/tutorial2_q3/tutorial2_q3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class TemperatureTracker
+{
+    private double[] temperatures;
+
+    public TemperatureTracker()
+    {
+        temperatures = new double[7];
+    }
+
+    public void InputTemperatures()
+    {
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            Console.Write($"Enter temperature for day {i + 1}: ");
+            temperatures[i] = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+
+    public void DisplayTemperatureReport()
+    {
+        Console.WriteLine("Weekly Temperature Report:");
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            Console.WriteLine($"Day {i + 1}: {temperatures[i]}°C");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TemperatureTracker tracker = new TemperatureTracker();
+        tracker.InputTemperatures();
+        tracker.DisplayTemperatureReport();
+    }
+}
