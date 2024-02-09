1ef426ff2b45ba4a31851ae1c51cdfc8c8dd5e6a
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-02-09 11:08:22 +0530

    tutorial2_q4

---
+++ tutorial-2/tutorial2_q4/tutorial2_q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Product
+{
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(string productName, decimal price)
+    {
+        ProductName = productName;
+        Price = price;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Product product1 = new Product("Orange", 14.99m);
+        Product product2 = new Product("Apple", 19.99m);
+
+        Console.WriteLine("Product 1: {0}, Price: {1}", product1.ProductName, product1.Price);
+        Console.WriteLine("Product 2: {0}, Price: {1}", product2.ProductName, product2.Price);
+    }
+}
