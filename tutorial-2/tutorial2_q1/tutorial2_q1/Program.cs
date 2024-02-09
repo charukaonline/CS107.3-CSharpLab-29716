cdc3dbf12d45b942b8e293f338433809aa0dbad6
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-02-09 10:37:33 +0530

    tutorial2_q1

---
+++ tutorial-2/tutorial2_q1/tutorial2_q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tutorial2_q1
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Title = "Madol Doowa";
+            book.Author = "Martin Wickramasinghe";
+
+            Console.WriteLine("Book Title: " + book.Title);
+            Console.WriteLine("Book Author: " + book.Author);
+        }
+    }
+}
