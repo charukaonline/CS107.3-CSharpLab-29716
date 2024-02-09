18269935e924b2bcf09bc4b3e594bca0f8e3bdb4
Charuka Karunarathna <130365250+charukaonline@users.noreply.github.com>
2024-02-09 10:48:29 +0530

    tutorial2_q2

---
+++ tutorial-2/tutorial2_q2/tutorial2_q2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BankAccountApp
+{
+    class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.AccountNumber = "123456789";
+            account.Balance = 1500;
+
+            decimal depositAmount = 500;
+            account.Deposit(depositAmount);
+
+            Console.WriteLine("Updated Balance: $" + account.Balance);
+        }
+    }
+}
